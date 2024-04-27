a6d984819126211a4a1e249ccc2eaf894f5419ef
dallko <150505854+dallko@users.noreply.github.com>
2024-04-27 23:47:43 +0600

    main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,20 @@
 
 using namespace std;
 
+string compressRLE(const string& data) {
+    string compressedData = "";
+    int count = 1;
+    for (int i = 0; i < data.length(); ++i) {
+        if (i + 1 < data.length() && data[i] == data[i + 1]) {
+            count++;
+        } else {
+            compressedData += to_string(count) + data[i];
+            count = 1;
+        }
+    }
+    return compressedData;
+}
+
 string decompressRLE(const string& data) {
     string decompressedData = "";
     int count = 0;
@@ -22,6 +36,34 @@ string decompressRLE(const string& data) {
         }
     }
     return decompressedData;
+}
+
+void writeCompressedData(const string& inputFile, const string& outputFile) {
+    ifstream inFile(inputFile);
+    ofstream outFile(outputFile);
+
+    if (!inFile) {
+        cerr << "Error: Unable to open input file!" << endl;
+        return;
+    }
+
+    if (!outFile) {
+        cerr << "Error: Unable to open output file!" << endl;
+        inFile.close();
+        return;
+    }
+
+    string data;
+    getline(inFile, data);
+
+    string compressedData = compressRLE(data);
+
+    outFile << compressedData;
+
+    inFile.close();
+    outFile.close();
+
+    cout << "Compression successful!" << endl;
 }
 
 void writeDecompressedData(const string& compressedFile, const string& outputFile) {
@@ -53,9 +95,11 @@ void writeDecompressedData(const string& compressedFile, const string& outputFil
 }
 
 int main() {
+    string inputFile = "input.txt";
     string compressedFile = "compressed.txt";
     string decompressedFile = "decompressed.txt";
 
+    writeCompressedData(inputFile, compressedFile);
     writeDecompressedData(compressedFile, decompressedFile);
 
     return 0;
